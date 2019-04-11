e1afa592177c79f1801821a404959cc6274dded3
lucau23 <luca@debian>
2019-04-11 23:33:57 +0200

    bomba1
    Signed-off-by: Luca <luca@debian>

--- SW_Analisys/data_plot.m
+++ SW_Analisys/data_plot.m
@@ -2,25 +2,29 @@ close all;
 clear;
 format short g
 %% Plot default 
-set(0,'DefaultLineLinewidth',2)%2
-set(0,'DefaultAxesFontSize',20)%20
-% set(0,'DefaultAxesXGrid','on',...
-% 'DefaultAxesYGrid','on',...
-% 'DefaultAxesZGrid','on')
-set(0,'DefaultTextInterpreter', 'latex')
-% warning('off','MATLAB:handle_graphics:exceptions:SceneNode') %no warning for error updating text
+% set(0,'DefaultLineLinewidth',2)%2
+% set(0,'DefaultAxesFontSize',20)%20
+% % set(0,'DefaultAxesXGrid','on',...
+% % 'DefaultAxesYGrid','on',...
+% % 'DefaultAxesZGrid','on')
+% set(0,'DefaultTextInterpreter', 'latex')
+% % warning('off','MATLAB:handle_graphics:exceptions:SceneNode') %no warning for error updating text
 %% Load Data
-load('20190407_pseudoStable_CollectiveTailSteps') %mettere indirizzo
+load('/home/luca/MEGA_Skunkworks/X-plane/Simulation_Log_Example/20190411_advanceAndZigZag.mat') %mettere indirizzo
 
+% filename = ['..//MEGA_Skunkworks//', ...
+%            'X-plane/Simulation_Log_Example/20190402_collectiveMainStep_1.mat'];
+% dataset = load(filename);
 %% Signals
 
 %%%----Inputs----
-
+try
 col_ts=logsout.find("u_collectiveMain_1").Values;
 lat_ts=logsout.find("u_aileron_1").Values;
 lon_ts=logsout.find("u_elevator_1").Values;
 ped_ts=logsout.find("u_collectiveTail_1").Values;
-
+catch
+end
 %%%----Outputs----
 
 %Body rotational velocity
@@ -51,24 +55,29 @@ v_z=logsout.find("y_Vz_1").Values; %[m/s];
 
 close all
 
-figure;plot(col_ts);ax(1)=gca;grid;
-figure;plot(lat_ts);ax(2)=gca;grid;
-figure;plot(lon_ts);ax(3)=gca;grid;
-figure;plot(lat_ts);ax(4)=gca;grid;
+%figure;plot(col_ts);ax(1)=gca;grid;
+%figure;plot(lat_ts);ax(2)=gca;grid;
+%figure;plot(lon_ts);ax(3)=gca;grid;
+%figure;plot(lat_ts);ax(4)=gca;grid;
 
-figure;plot(p_ts);ax(5)=gca;grid;
-figure;plot(q_ts);ax(6)=gca;grid;
-figure;plot(r_ts);ax(7)=gca;grid;
-figure;plot(phi_ts);ax(8)=gca;grid;
-figure;plot(theta_ts);ax(9)=gca;grid;
-figure;plot(psi_ts);ax(10)=gca;grid;
+figure;
+subplot(1,3,1);plot(p_ts);ax(5)=gca;grid;
+subplot(1,3,2);plot(q_ts);ax(6)=gca;grid;
+subplot(1,3,3);plot(r_ts);ax(7)=gca;grid;
 
-figure;plot(p_x);ax(11)=gca;grid;
-figure;plot(p_y);ax(12)=gca;grid;
-figure;plot(p_z);ax(13)=gca;grid;
-figure;plot(v_x);ax(11)=gca;grid;
-figure;plot(v_y);ax(12)=gca;grid;
-figure;plot(v_z);ax(13)=gca;grid;
+figure;
+subplot(1,3,1);plot(phi_ts);ax(8)=gca;grid;
+subplot(1,3,2);plot(theta_ts);ax(9)=gca;grid;
+subplot(1,3,3);plot(psi_ts);ax(10)=gca;grid;
+
+figure;
+subplot(1,3,1);plot(p_x);ax(11)=gca;grid;
+subplot(1,3,2);plot(p_y);ax(12)=gca;grid;
+subplot(1,3,3);plot(p_z);ax(13)=gca;grid;
+
+figure;
+subplot(1,3,1);plot(v_x);ax(11)=gca;grid;
+subplot(1,3,2);plot(v_y);ax(12)=gca;grid;
+subplot(1,3,3);plot(v_z);ax(13)=gca;grid;
 
 linkaxes(ax,'x');
-
